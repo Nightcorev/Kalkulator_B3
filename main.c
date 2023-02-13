61ff2e80b86f864e55400f975fcf96b669bb8cf6
fikm21 <taufikm211@gmail.com>
2023-02-13 11:32:26 +0700

    update kalkulator sementara

--- main.c
+++ main.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <math.h>
-
 #include "kalkulator.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	
-	int pilih_menu,input_angka1,input_angka2,hasil;
-	double input_log, hasil_log;
-	
+	int pilih_menu;
+	char kembali;
+
 	menu_utama();
 	printf("\n\t\t\tPilih Menu : ");
 	scanf("%d",&pilih_menu);
@@ -18,36 +17,7 @@ int main(int argc, char *argv[]) {
 	switch(pilih_menu){
 		case 1 :
 			menu_kalkulator_scientifik();
-			printf("\n\t\t\tPilih Menu : ");
-			scanf("%d",&pilih_menu);
-			switch(pilih_menu){
-				case 1:
-					system("cls");
-					pertambahan_kurang();
-					break;
-				case 2: 
-					system("cls");
-					input_log = inputLogarithm();
-					hasil_log = logarithm(input_log);
-					resultLogarithm(hasil_log);
-					break;
-				case 3:
-					system("cls");
-					trigonometri();
-					break;
-				case 4:
-					system("cls");
-					scanf("%d %d",&input_angka1,&input_angka2);
-					hasil = perpangkatan (input_angka1,input_angka2);
-					printf("hasil : %d",hasil);
-					break;
-				default:
-					printf("\n\t\t\tSalah Input !!");
-					menu_kalkulator_scientifik();
-					break;
-				
-				
-			}
+			proses_kalkulator();
 			break;
 		case 2 :
 			menu_kalkulator_konversi();
--- taufik.h
+++ taufik.h
@@ -1,6 +1,7 @@
 #ifndef taufik_h
 #define taufik_h
 #include <stdio.h>
+#include "kalkulator.h"
 
 //Deklarasi Modul Tampilan
 void menu_utama();
@@ -31,19 +32,13 @@ void menu_utama(){
 void menu_kalkulator_scientifik(){
 	system("cls");
 	printf("\n\n\n");
-	printf("\n\t\t\t-------------------------------");
-	printf("\n\t\t\t|    Scientifik Kalkulator    |");
-	printf("\n\t\t\t|         Kelompok B3         |");
-	printf("\n\t\t\t|-----------------------------|");
-	printf("\n\t\t\t|        Menu Scientifik      |");
-	printf("\n\t\t\t|                             |");
-	printf("\n\t\t\t| 1. Tambah & Kurang          |");
-	printf("\n\t\t\t| 2. Logaritma                |");
-	printf("\n\t\t\t| 3. Trigonometri             |");
-	printf("\n\t\t\t| 4. Perpangkatan             |");
-	printf("\n\t\t\t| 5. Kembali                  |");
-	printf("\n\t\t\t|                             |");
-	printf("\n\t\t\t-------------------------------");
+	printf("\n\t\t\t------------------------------------------");
+	printf("\n\t\t\t|    Scientifik Kalkulator               |");
+	printf("\n\t\t\t|         Kelompok B3                    |");
+	printf("\n\t\t\t|----------------------------------------|");
+	printf("\n\t\t\t| Keterangan :                           |");
+	printf("\n\t\t\t| (^) = untuk operasi perpangkatan       |");
+	printf("\n\t\t\t------------------------------------------");
 }
 
 void menu_kalkulator_konversi(){
@@ -59,5 +54,88 @@ void menu_kalkulator_konversi(){
 	printf("\n\t\t\t| 2. Kembali                  |");
 	printf("\n\t\t\t|                             |");
 	printf("\n\t\t\t-------------------------------");
+}
+
+double perform_operation(double num1, double num2, char operator) {
+    switch (operator) {
+        case '^':
+            return pow(num1, num2);
+        case '*':
+            return num1 * num2;
+        case '/':
+            return num1 / num2;
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        default:
+            printf("Invalid operator: %c", operator);
+            exit(1);
+    }
+}
+
+int get_priority(char operator) {
+    switch (operator) {
+        case '^':
+            return 3;
+        case '*':
+        case '/':
+            return 2;
+        case '+':
+        case '-':
+            return 1;
+        default:
+            printf("Invalid operator: %c", operator);
+            exit(1);
+    }
+}
+
+void proses_kalkulator(){
+	char expression[100];
+    double stack_num[100];
+    int stack_num_top = -1;
+    char stack_op[100];
+    int stack_op_top = -1;
+    int i;
+			printf("\n\t\t\tMasukkan expresi: ");
+    		scanf("%s", expression);
+    		 for (i = 0; expression[i]; i++) {
+	        if (isdigit(expression[i])) {
+	            char number[100];
+	            int number_top = 0;
+	            while (isdigit(expression[i]) || expression[i] == '.') {
+	                number[number_top++] = expression[i++];
+	            }
+	            number[number_top] = '\0';
+	            stack_num[++stack_num_top] = strtod(number, NULL);
+	            i--;
+	        } else if (expression[i] == '(') {
+	            stack_op[++stack_op_top] = expression[i];
+	        } else if (expression[i] == ')') {
+	            while (stack_op[stack_op_top] != '(') {
+	                double num2 = stack_num[stack_num_top--];
+	                double num1 = stack_num[stack_num_top--];
+	                char operator = stack_op[stack_op_top--];
+	                stack_num[++stack_num_top] = perform_operation(num1, num2, operator);
+	            }
+	            stack_op_top--;
+	        } else {
+	            while (stack_op_top >= 0 && get_priority(stack_op[stack_op_top]) >= get_priority(expression[i])) {
+	                double num2 = stack_num[stack_num_top--];
+	                double num1 = stack_num[stack_num_top--];
+	                char operator = stack_op[stack_op_top--];
+	                stack_num[++stack_num_top] = perform_operation(num1, num2, operator);
+	            }
+	            stack_op[++stack_op_top] = expression[i];
+			}
+			}
+			while (stack_op_top >= 0) {
+			    double num2 = stack_num[stack_num_top--];
+			    double num1 = stack_num[stack_num_top--];
+			    char operator = stack_op[stack_op_top--];
+			    stack_num[++stack_num_top] = perform_operation(num1, num2, operator);
+			}
+		
+			printf("\t\t\tResult: %g\n", stack_num[0]);
 }
 #endif
