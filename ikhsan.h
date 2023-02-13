00d2e5394d582160bcd7217d8076669e1707d1db
Ikhsan310105 <ini.ikhsan31@gmail.com>
2023-02-13 12:52:14 +0700

    file ikhsan 2

--- ikhsan.h
+++ ikhsan.h
@@ -7,9 +7,11 @@
 
 const float sudutSegitiga=180.0;
 float trigonometri(double angleTrigonometri, char opsiTrigonometri);
+float konversiSuhu();
 float sinHasil(int angleTrigonometri);
 float cosHasil(int angleTrigonometri);
 float tanHasil(int angleTrigonometri);
+float operasiSuhu(int opsiSuhu1, int opsiSuhu2, float suhu);
 
 float trigonometri(double angleTrigonometri, char opsiTrigonometri){
 	float hasilTrigonometri;
@@ -58,90 +60,86 @@ float tanHasil(int angleTrigonometri){
 
 float konversiSuhu(){
 	int opsiSuhu1,opsiSuhu2;
-	float suhu;
+	float suhu,hasilSuhu;
 	printf("Pilih suhu awal\n");
 	printf("1. Celcius\n2. Fahrenheit\n3. Reaumur\n4. Kelvin\n");
 	scanf("%d",&opsiSuhu1);
 	printf("Masukkan suhu : ");
-	scanf("%d",&suhu);
+	scanf("%f",&suhu);
 	printf("1. Celcius\n2. Fahrenheit\n3. Reaumur\n4. Kelvin\n");
 	scanf("%d",&opsiSuhu2);
+	hasilSuhu=operasiSuhu(opsiSuhu1,opsiSuhu2,suhu);
+	printf("hasil konversinya adalah %.2f",hasilSuhu);
+	return 0;
+}
+
+float operasiSuhu(int opsiSuhu1, int opsiSuhu2, float suhu){	
 	switch(opsiSuhu1){
 		case 1:
-			break;
-			switch(opsiSuhu2){
-				case 1:
-					return suhu;
-					break;
-				case 2:
-					suhu=((suhu*9)/5)+32;
-					return suhu;
-					break;
-				case 3:
-					suhu=(suhu*4)/5;
-					return suhu;
-					break;
-				case 4:
-					suhu=suhu+273.15;
-					return suhu;
+			if(opsiSuhu2==1){
+				return suhu;
+			}else if(opsiSuhu2==2){
+				suhu=((suhu*9)/5)+32;
+				return suhu;
+			}else if(opsiSuhu2==3){
+				suhu=(suhu*4)/5;
+				return suhu;
+			}else if(opsiSuhu2==4){
+				suhu=suhu+273.15;
+				return suhu;
+			}else{
+				exit(1);
 			}
+			break;
 		case 2:
-			break;
-			switch(opsiSuhu2){
-				case 1:
-					suhu=((suhu-32)*5)/9;
-					return suhu;
-					break;
-				case 2:
-					return suhu;
-					break;
-				case 3:
-					suhu=((suhu-32)*4)/9;
-					return suhu;
-					break;
-				case 4:
-					suhu=(((suhu-32)*5)/9)+273.15;
-					return suhu;
-			}
+			if(opsiSuhu2==1){
+				suhu=((suhu-32)*5)/9;
+				return suhu;
+			}else if(opsiSuhu2==2){
+				return suhu;
+			}else if(opsiSuhu2==3){
+				suhu=((suhu-32)*4)/9;
+				return suhu;
+			}else if(opsiSuhu2==4){
+				suhu=(((suhu-32)*5)/9)+273.15;
+				return suhu;
+			}else{
+				exit(1);
+			}			
 			break;
 		case 3:
-			switch(opsiSuhu2){
-				case 1:
-					suhu=(suhu*5)/4;
-					return suhu;
-					break;
-				case 2:
-					suhu=((suhu*9)/4)+32;
-					return suhu;
-					break;
-				case 3:
-					return suhu;
-					break;
-				case 4:
-					suhu=((suhu*5)/4)+273.15;
-					return suhu;
-			}
+			if(opsiSuhu2==1){
+				return suhu;
+			}else if(opsiSuhu2==2){
+				suhu=((suhu*9)/5)+32;
+				return suhu;
+			}else if(opsiSuhu2==3){
+				suhu=(suhu*4)/5;
+				return suhu;
+			}else if(opsiSuhu2==4){
+				suhu=suhu+273.15;
+				return suhu;
+			}else{
+				exit(1);
+			}			
 			break;
 		case 4:
-			switch(opsiSuhu2){
-				case 1:
-					suhu=suhu-273.15;
-					return suhu;
-					break;
-				case 2:
-					suhu=(((suhu-273.15)*9)/5)+32;
-					return suhu;
-					break;
-				case 3:
-					suhu=((suhu-273.5)*4)/5;
-					break;
-				case 4:
-					return suhu;
-			}
+			if(opsiSuhu2==1){
+				suhu=suhu-273.15;
+				return suhu;
+			}else if(opsiSuhu2==2){
+				suhu=(((suhu-273.15)*9)/5)+32;
+				return suhu;
+			}else if(opsiSuhu2==3){
+				suhu=((suhu-273.5)*4)/5;
+			}else if(opsiSuhu2==4){
+				return suhu;
+			}else{
+				exit(1);
+			}			
 			break;			
 	}
 }
-
 
 
 #endif
