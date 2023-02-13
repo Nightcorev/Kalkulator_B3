303ab7905e00ea14862ae29b19a6d7c5b9b5823c
Ikhsan310105 <ini.ikhsan31@gmail.com>
2023-02-13 12:25:35 +0700

    file ikhsan 2

--- ikhsan.h
+++ ikhsan.h
@@ -6,45 +6,36 @@
 #include <stdlib.h>
 
 const float sudutSegitiga=180.0;
-void trigonometri();
+float trigonometri(double angleTrigonometri, char opsiTrigonometri);
 float sinHasil(int angleTrigonometri);
 float cosHasil(int angleTrigonometri);
 float tanHasil(int angleTrigonometri);
 
-void trigonometri(){
+float trigonometri(double angleTrigonometri, char opsiTrigonometri){
 	float hasilTrigonometri;
-	int opsiTrigonometri;
-	int angleTrigonometri;
-	char opsiKembali;
-	printf("Pilih Trigonometri\n");
-	printf("1. Sin\n2. Cos\n3. Tan\n");
-	printf("masukkan opsi = ");
-	scanf("%d",&opsiTrigonometri);
-	printf("Masukkan sudut = ");
-	scanf("%d",&angleTrigonometri);
 	switch(opsiTrigonometri){
-		case 1:
+		case 's':
 			hasilTrigonometri=sinHasil(angleTrigonometri);
+			return hasilTrigonometri;
 			break;
-		case 2:
+		case 'c':
 			hasilTrigonometri=cosHasil(angleTrigonometri);
+			return hasilTrigonometri;
 			break;
-		case 3:
+		case 't':
 			hasilTrigonometri=tanHasil(angleTrigonometri);
-			break;
+			return hasilTrigonometri;
 			break;
 		default:
 			exit(1);
 			break;
 	}
-	printf("Hasilnya adalah %.2f",hasilTrigonometri);
-	fflush;
 }
 
 float sinHasil(int angleTrigonometri){
 	float hasilSin;
 	
-	hasilSin=angleTrigonometri*phi/sudutSegitiga;
+	hasilSin=(angleTrigonometri*phi)/sudutSegitiga;
 	hasilSin=sin(hasilSin);
 	return hasilSin;
 }
@@ -52,7 +43,7 @@ float sinHasil(int angleTrigonometri){
 float cosHasil(int angleTrigonometri){
 	float hasilCos;
 	
-	hasilCos=angleTrigonometri*phi/sudutSegitiga;
+	hasilCos=(angleTrigonometri*phi)/sudutSegitiga;
 	hasilCos=cos(hasilCos);
 	return hasilCos;
 }
@@ -60,9 +51,97 @@ float cosHasil(int angleTrigonometri){
 float tanHasil(int angleTrigonometri){
 	float hasilTan;
 	
-	hasilTan=angleTrigonometri*phi/sudutSegitiga;
+	hasilTan=(angleTrigonometri*phi)/sudutSegitiga;
 	hasilTan=tan(hasilTan);
 	return hasilTan;
 }
+
+float konversiSuhu(){
+	int opsiSuhu1,opsiSuhu2;
+	float suhu;
+	printf("Pilih suhu awal\n");
+	printf("1. Celcius\n2. Fahrenheit\n3. Reaumur\n4. Kelvin\n");
+	scanf("%d",&opsiSuhu1);
+	printf("Masukkan suhu : ");
+	scanf("%d",&suhu);
+	printf("1. Celcius\n2. Fahrenheit\n3. Reaumur\n4. Kelvin\n");
+	scanf("%d",&opsiSuhu2);
+	switch(opsiSuhu1){
+		case 1:
+			break;
+			switch(opsiSuhu2){
+				case 1:
+					return suhu;
+					break;
+				case 2:
+					suhu=((suhu*9)/5)+32;
+					return suhu;
+					break;
+				case 3:
+					suhu=(suhu*4)/5;
+					return suhu;
+					break;
+				case 4:
+					suhu=suhu+273.15;
+					return suhu;
+			}
+		case 2:
+			break;
+			switch(opsiSuhu2){
+				case 1:
+					suhu=((suhu-32)*5)/9;
+					return suhu;
+					break;
+				case 2:
+					return suhu;
+					break;
+				case 3:
+					suhu=((suhu-32)*4)/9;
+					return suhu;
+					break;
+				case 4:
+					suhu=(((suhu-32)*5)/9)+273.15;
+					return suhu;
+			}
+			break;
+		case 3:
+			switch(opsiSuhu2){
+				case 1:
+					suhu=(suhu*5)/4;
+					return suhu;
+					break;
+				case 2:
+					suhu=((suhu*9)/4)+32;
+					return suhu;
+					break;
+				case 3:
+					return suhu;
+					break;
+				case 4:
+					suhu=((suhu*5)/4)+273.15;
+					return suhu;
+			}
+			break;
+		case 4:
+			switch(opsiSuhu2){
+				case 1:
+					suhu=suhu-273.15;
+					return suhu;
+					break;
+				case 2:
+					suhu=(((suhu-273.15)*9)/5)+32;
+					return suhu;
+					break;
+				case 3:
+					suhu=((suhu-273.5)*4)/5;
+					break;
+				case 4:
+					return suhu;
+			}
+			break;			
+	}
+}
+
+
 
 #endif
