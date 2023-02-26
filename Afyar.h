2019f9964a12d09ee2b1888d67bd57378ba551be
afyar08 <117814143+afyar08@users.noreply.github.com>
2023-02-26 14:28:37 +0700

    Update Afyar.h
    Signed-off-by: afyar08 <117814143+afyar08@users.noreply.github.com>

--- Afyar.h
+++ Afyar.h
@@ -2,14 +2,14 @@
 #define Afyar_h
 #include <math.h>
 
-int perpangkatan (int x, int y);
-float hitungPersentase(float angka, float persen);
+double perpangkatan (double x, double y);
+double hitungPersentase(double angka, double persen);
 double akarKuadrat(double x);
 double hitung_phi(double input);
 
 
-int perpangkatan (int x, int y){
-	int i, a;
+double perpangkatan (double x, double y){
+	double i, a;
 	a=x;
 	
 	for (i=1; i<y; i++){
@@ -19,7 +19,7 @@ int perpangkatan (int x, int y){
 }
 
 
-float hitungPersentase(float angka, float persen) {
+double hitungPersentase(double angka, double persen) {
     return angka * (persen / 100);
 }
 
