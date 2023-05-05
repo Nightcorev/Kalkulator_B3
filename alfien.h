43bccdf597837730bf376f95e164dcdabac76a81
alfiensukma <alfien.sukmaprawira1724@gmail.com>
2023-05-05 21:20:44 +0700

    [Update] Header Alfien, [Add] Header Main

--- alfien.h
+++ alfien.h
@@ -9,6 +9,9 @@
 /*Daftar modul*/
 double logarithm(double number, double base);
 double naturalLogarithm(double number);
+double processLogarithm(double number, double base, char *op);
+node CreateNodeList();
+address BuildTree(infotype postfix[]);
 
 //rumus logaritma
 double logarithm(double number, double base)
@@ -33,8 +36,46 @@ double processLogarithm(double number, double base, char *op){
 		return naturalLogarithm(number);
 	}
 	else{
-		printf("Operator is invalid: %s", op);
+		printf("\t\tOperator is invalid: %s", op);
         exit(1);
 	}
+}
+
+//Create Node
+node CreateNodeList(){
+	node P;
+	
+	P = (node) malloc (sizeof (ElmtList));
+	if(P==NULL){
+		printf("Gagal Alokasi");
+	}else{
+		(P)->next=NULL;
+		(P)->isoperator=1;
+	} 
+	
+	return P;
+}
+
+//build tree
+address BuildTree(infotype postfix[]){
+	address P;
+	address stack[50];
+	int i, len, top=-1;
+	infotype c;
+	
+	len=strlen(postfix);
+	
+	for(i=0;i<len;i++){
+		c=postfix[i];
+		if(isdigit(c)){
+			P=CreateNode(c);
+		} else{
+			P=CreateNode(c);
+			right(P)=stack[top--];
+			left(P)=stack[top--];
+		}
+		stack[++top]=P;
+	}
+	return(stack[0]);
 }
 #endif
