f11a413465f4e94400d0fb3233ca47d5a9692b7c
zeezkra <ziadzikra02@gmail.com>
2023-05-21 19:41:52 +0700

    [UPDATE] fix & comment

--- Zia.h
+++ Zia.h
@@ -39,25 +39,27 @@ double pembagian(double bil1, double bil2){
 	return bil1 / bil2;
 }
 
-address CreateNodeOperand(float input){
-	address P;
-	P = (address) malloc (sizeof (Tree));
-	P->operand=input;
-	P->isOperator=0;
-	P->left=NULL;
-	P->right=NULL;
-	return P;
-	
+//Modul Create Node Operand
+address CreateNodeOperand(float input){ 
+	address P; 
+	P = (address) malloc (sizeof (Tree)); //mengalokasikan memori bertipe data Tree menggunakan fungsi malloc.
+	P->operand=input;                     //Memperbarui nilai operand di node yang ditunjuk oleh P dengan nilai dari parameter input.
+	P->isOperator=0;                      //Mengatur nilai isOperator di node yang ditunjuk oleh P menjadi 0
+	P->left=NULL;                         //Menginisialisasi pointer left di node yang ditunjuk oleh P dengan NULL
+	P->right=NULL;                        //Menginisialisasi pointer right di node yang ditunjuk oleh P dengan NULL
+	return P;                             //Mengembalikan alamat dari node
+	 
 }
 
-address CreateNodeOperator(char input){
+//Modul Create Node Operator
+address CreateNodeOperator(char input){   //Fungsi ini mengambil satu parameter input bertipe data "char".
 	address P;
-	P = (address) malloc (sizeof (Tree));
-	P->data=input;
-	P->isOperator=1;
+	P = (address) malloc (sizeof (Tree)); //Baris ini mengalokasikan memori untuk objek bertipe data Node menggunakan fungsi malloc.
+	P->data=input;                        
+	P->isOperator=1;                      //Mengatur nilai isOperator di node yang ditunjuk oleh P menjadi 1
 	P->left=NULL;
 	P->right=NULL;
-	return P;
+	return P;                             //Mengembalikan alamat dari node
 	
 }  
 
