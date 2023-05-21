c5cf456fd3f8efd8911438950c61b9543c468dcd
fikm21 <taufikm211@gmail.com>
2023-05-21 22:22:45 +0700

    fix menu dan add comment

--- main.c
+++ main.c
@@ -16,31 +16,33 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	/* Deklarasi variabel dan struct yang akan digunakan */
 	double hasil;
-	char input_expresi[100];
+	char input_expresi[255];
 	char kembali,temp;
 	Queue ListQueue;
 	node Node;
 	
-	do{
+	do{ //Melakukan looping selama kembali bernilai t atau T
 		system("cls");
 		address root;
 		ListQueue.First=NULL;
 		ListQueue.Last=NULL;
 		kembali='t';
 		
-		menu_kalkulator_scientifik();
+		menu_kalkulator_scientifik(); // Menampilkan menu kalkulator yang diambil dari taufik.h
 		printf("\n\t\t\tMasukkan expresi: ");fflush(stdin);
-    	scanf("%s", input_expresi);
+    	scanf("%s", input_expresi); // Menginputkan expresi yang di simpan di input_expresi
     	fflush(stdin);
-    	proses_kalkulator(&ListQueue,input_expresi,&kembali);
-    	if(kembali=='t'){
-			root = Create_Tree(ListQueue);
-			hasil = proses_operasi(root);
-			printf("\n\t\t\tHasil : %g",hasil);
+    	proses_kalkulator(&ListQueue,input_expresi,&kembali); // Melakukan proes infix to postfix yang akan disimpan di list Queue dari taufik.h
+    	if(kembali=='t'){ // Mengecek apakah kembali bernilai t
+			root = Create_Tree(ListQueue); // membuat tree berdasarkan list Queue dari alfien.h
+			hasil = proses_operasi(root); // menghitung atau melakukan operasi sesuai dengan yang ada di tree yang disimpan di hasil dari taufik.h
+			printf("\n\t\t\tHasil : %g",hasil); // menampilkan hasil yang telah dioperasikan
 			printf("\n\t\t\tKeluar (y/t) : ");fflush(stdin);
-			scanf("%c",&kembali);	
-		}else{
+			scanf("%c",&kembali);	// memilih akan keluar atau mengulang kalkulator
+		}else{ 
+			// jika bukan maka akan menampilkan Math error dan dapat memilih akan keluar atau tidak
 			printf("\t\t\tMATH ERROR");
 			printf("\n\t\t\tKeluar (y/t) : ");fflush(stdin);
 			scanf("%c",&kembali);	
--- taufik.h
+++ taufik.h
@@ -6,12 +6,14 @@
 #include "alfien.h"
 #include "Zia.h"
 #include "Fikri.h"
-#define PHI 1.6180339887
+
 
 //Deklarasi Modul Tampilan
 void menu_kalkulator_scientifik();
 double proses_operasi(address Troot);
-void proses_kalkulator(Queue *listQ, char* input, char* kembali ); 
+void EnqueOperator(Queue *listQ,char item);
+void EnqueOperand(Queue *Q,float item);
+void proses_kalkulator(Queue *listQ, char* input, char* kembali);
 
 //Isi Program
 
@@ -23,9 +25,11 @@ void menu_kalkulator_scientifik(){
 	printf("\n\t\t\t|----------------------------------------------------|");
 	printf("\n\t\t\t| Keterangan :                                       |");
 	printf("\n\t\t\t| (^) = untuk operasi perpangkatan                   |");
-	printf("\n\t\t\t| (p) = untuk operasi Phi                            |");
 	printf("\n\t\t\t| (m) = untuk operasi modulus                        |");
-	printf("\n\t\t\t| (&) = untuk operasi akar pangkat n                 |");
+	printf("\n\t\t\t| (v) = untuk operasi akar pangkat n                 |");
+	printf("\n\t\t\t| (log) = untuk operasi logaritma                    |");
+	printf("\n\t\t\t| (ln) = untuk operasi natural logaritma             |");
+	printf("\n\t\t\t| (!) = untuk operasi faktorial                      |");
 	printf("\n\t\t\t| (%%) = untuk operasi hitung persentase              |");
 	printf("\n\t\t\t|                                                    |");
 	printf("\n\t\t\t| Guide :                                            |");
@@ -36,44 +40,63 @@ void menu_kalkulator_scientifik(){
 	printf("\n\t\t\t|    contohnya seperti berikut : 2log(3)             |");
 	printf("\n\t\t\t| 4. untuk operasi menghitung persentase             |");
 	printf("\n\t\t\t|    cara menggunakannya seperti berikut : 20%%100=20%%|");
+	printf("\n\t\t\t| 5. untuk operasi faktorial                         |");
+	printf("\n\t\t\t|    contohnya seperti berikut : 2!                  |");
 	printf("\n\t\t\t------------------------------------------------------");
 }
 
 double proses_operasi(address Troot){
-	if(Troot->isOperator==1){
-		if(Troot->data=='+'){
-			return perjumlahan(proses_operasi(Troot->left), proses_operasi(Troot->right));
-		}else if(Troot->data=='-'){
-			return pengurangan(proses_operasi(Troot->left), proses_operasi(Troot->right));
-		}else if(Troot->data=='/'){
-			return pembagian(proses_operasi(Troot->left), proses_operasi(Troot->right));
-		}else if(Troot->data=='*'){
+	if(Troot->isOperator==1){ // Mengecek apakah Troot ialah operator atau bukan
+		if(Troot->data=='+'){ //Mengecek apakah subvar data pada Troot ialah karakter '+'
+			// jika iya akan melakuan proses perjumlahan yang diambil dari Zia.h lalu mereturn hasilnya
+			return perjumlahan(proses_operasi(Troot->left), proses_operasi(Troot->right)); 
+			
+		}else if(Troot->data=='-'){ //Mengecek apakah subvar data pada Troot ialah karakter '-'
+			// jika iya akan melakuan proses pengurangan yang diambil dari Zia.h lalu mereturn hasilnya
+			return pengurangan(proses_operasi(Troot->left), proses_operasi(Troot->right)); 
+			
+		}else if(Troot->data=='/'){ //Mengecek apakah subvar data pada Troot ialah karakter '/'
+			// jika iya akan melakuan proses pembagian yang diambil dari Zia.h lalu mereturn hasilnya
+			return pembagian(proses_operasi(Troot->left), proses_operasi(Troot->right));			
+			
+		}else if(Troot->data=='*'){ //Mengecek apakah subvar data pada Troot ialah karakter '*'
+			// jika iya akan melakuan proses perkalian yang diambil dari Zia.h lalu mereturn hasilnya
 			return perkalian(proses_operasi(Troot->left), proses_operasi(Troot->right));
-		}else if(Troot->data=='^'){
+			
+		}else if(Troot->data=='^'){ //Mengecek apakah subvar data pada Troot ialah karakter '^'
+			// jika iya akan melakuan proses perpangkatan yang diambil dari Afyar.h lalu mereturn hasilnya
 			return perpangkatan(proses_operasi(Troot->left), proses_operasi(Troot->right));
-		}else if(Troot->data=='m'){
+			
+		}else if(Troot->data=='v'){ //Mengecek apakah subvar data pada Troot ialah karakter '&'
+			// jika iya akan melakuan proses hitung_akar yang diambil dari Afyar.h lalu mereturn hasilnya
+			return hitung_akar(proses_operasi(Troot->left), proses_operasi(Troot->right)); 
+			
+		}else if(Troot->data=='m'){ //Mengecek apakah subvar data pada Troot ialah karakter 'm'
+			// jika iya akan melakuan proses modulus yang diambil dari Fikri.h lalu mereturn hasilnya
 			return modulus(proses_operasi(Troot->left), proses_operasi(Troot->right));
-		}else if(Troot->data=='%'){
+			
+		}else if(Troot->data=='%'){ //Mengecek apakah subvar data pada Troot ialah karakter '%'
+			// jika iya akan melakuan proses hitungPersentase yang diambil dari Afyar.h lalu mereturn hasilnya
 			return hitungPersentase(proses_operasi(Troot->left), proses_operasi(Troot->right));   
-		}else if(Troot->data=='v'){
-			return hitung_akar(proses_operasi(Troot->right), proses_operasi(Troot->left));
+			
 		}else{
 			 printf("\n\t\t\tInvalid operator ");
 		}
 	}
+	// Jika Troot bukan operator makan akan mereturn bilangan operand
 	return Troot->operand;
 }
 
 void EnqueOperator(Queue *listQ,char item){
 	node P;
 	
-	P=CreateNodeList();
-	P->oprtr=item;
-	if(listQ->First==NULL){
+	P=CreateNodeList(); //Memanggil modul CrateNodeList dari alfien.h untuk membuat node queue baru
+	P->oprtr=item; //memasukkan subvar oprtr dari subvar node P dengan item
+	if(listQ->First==NULL){ // Mengecek apakah subvar dari listQ bernilai NULL jika iya First dan Last akan menunjuk node P dan next Last akan berisi Null
 		listQ->First=P;
 		listQ->Last=P;
 		listQ->Last->next=NULL;
-	} else{
+	} else{ // Jika tidak subvar next pada Last akan menunjuk node P dan Last akan berpindah pada node P 
 		P->next=NULL;
 		listQ->Last->next=P;
 		listQ->Last=P;
@@ -83,14 +106,14 @@ void EnqueOperator(Queue *listQ,char item){
 void EnqueOperand(Queue *Q,float item){
 	node P;
 	
-	P=CreateNodeList();
-	P->operand=item;
-	P->isoperator=0;
-	if(Q->First==NULL){
+	P=CreateNodeList(); //Memanggil modul CrateNodeList dari alfien.h untuk membuat node queue baru
+	P->operand=item; //memasukkan subvar operand dari subvar node P dengan item
+	P->isoperator=0; //subvar isOperator pada node P akan diberi nilai 0 yang menunjukan bukan Operator
+	if(Q->First==NULL){ // Mengecek apakah subvar dari listQ bernilai NULL jika iya First dan Last akan menunjuk node P dan next Last akan berisi Null
 		Q->First=P;
 		Q->Last=P;
 		Q->Last->next=NULL;
-	} else{
+	} else{ // Jika tidak subvar next pada Last akan menunjuk node P dan Last akan berpindah pada node P 
 		P->next=NULL;
 		Q->Last->next=P;
 		Q->Last=P;
@@ -98,6 +121,10 @@ void EnqueOperand(Queue *Q,float item){
 }
 
 void proses_kalkulator(Queue *listQ, char* input, char* kembali){
+	/*referensi https://en.wikipedia.org/wiki/Shunting_yard_algorithm#:~:text=In%20computer%20science%2C%20the%20shunting,abstract%20syntax%20tree%20(AST). 
+	Proses Infix to Postfix*/
+	
+	
 	node P;
 	Stack listS;
 	char token,oprtr,negatif;
@@ -109,86 +136,102 @@ void proses_kalkulator(Queue *listQ, char* input, char* kembali){
 	
 	listS.Head=NULL;
 	for(i=0;i<strlen(input);i++){
-		token=input[i];
-		if(isdigit(token)||token=='.'||(token=='-'&&(isOperator(input[i-1])||i==0||input[i-1]=='('))){
-			if(isdigit(token)){
-				num=num*10+(token-'0');
-			}else if(token=='.'){
+		token = input[i]; // Inisialisasi token dengan nilai yang diinputkan satu per satu
+
+		// Mengecek apakah token merupakan digit atau tanda negatif pada bilangan
+		if (isdigit(token) || token == '.' || (token == '-' && (isOperator(input[i - 1]) || i == 0 || input[i - 1] == '('))){
+			if (isdigit(token)){ // Mengecek apakah token merupakan digit
+				num = num * 10 + (token - '0'); // jika iya akan dihitung lalu diubah menjadi nilai numerik yang sesuai mengunakan konversi ASCII
+			}
+			else if (token == '.'){ //Mengecek apakah token adalah tanda desimal
 				i++;
-				while(isdigit(input[i])){
-				num2=(input[i]-'0');
-				num=num+(num2/num3);	
-				num3=num3*10;
-				i++;
+				while (isdigit(input[i])){ // Melakukan looping dari . hingga bukan bilangan desimal
+					num2 = (input[i] - '0'); // Mengambil nilai setelah tanda desimal menjadi nilai numerik
+					num = num + (num2 / num3); // Menggabungkan angka sebelum tanda desimal dengan yang sesudah desimal
+					num3 = num3 * 10; //Memperbarui nilai pembagi dengan mengalikan 10 agar dapat memproses digit-desimal selanjutnya dengan benar
+					i++;
+				}
+				num3 = 10; // Mereset ulang nilai pembagi dengan 10 agar bisa nanti digunakan lagi
+				i--;
 			}
-			num3=10;
-			i--;
-			}else if(token=='-'){
-				negatif='-';
+			else if (token == '-'){ // Mengecek apakah token - jika iya maka negatif diisi - yang menandakan bilangan negatif
+				negatif = '-';
 			}
-			 if(isdigit(input[i+1])!=1&&input[i+1]!='.'){
-			 	if(negatif=='-'){
-			 		num=num*-1;
-				 }
-				negatif='\0';
-				EnqueOperand(&*listQ, num);
-				num=0;
+
+			// Jika token berikutnya bukan digit atau titik desimal, maka bilangan selesai dibaca dan dimasukkan ke Queue
+			if (isdigit(input[i + 1]) != 1 && input[i + 1] != '.'){
+				if (negatif == '-'){ // Mengecek apakah bilangan negatif jika iya akan dikalikan -1 untuk mendapatkan nilai negatif
+					num = num * -1;
+				}
+				negatif = '\0'; //Mereset variabel negatif menjadi variabel kosong
+				EnqueOperand(&*listQ, num); // Melakukan proeses Enque atau menambahkan node ke Queue
+				num = 0; //Mereset num menjadi 0
 			}
-		}else if (token=='s' || token=='c' || token=='t'){
+		}else if (token=='s' || token=='c' || token=='t'){ 		// Jika token merupakan operator trigonometri (s, c, t, a)
 			char trigono[5];
 			char sudut[20];
 			int x=0;
 			int j=0;
 			float hasil;
 			double angka1;
+			
+			// memeriksa apakah karakter sebelum operator trigonometri bukan digit
 			if(!isdigit(input[i-1])){
-				while(input[i]!=')'&&i<strlen(input)){
-					if(isdigit(input[i]) || input[i]=='.'){
+				while(input[i]!=')'&&i<strlen(input)){ 
+				//  loop akan membaca karakter-karakter setelah operator trigonometri hingga menemui tanda kurung tutup (')') atau akhir dari input.
+				
+					if(isdigit(input[i]) || input[i]=='.'){ // Mengecek apakah karakter saat ini merupakan digit atau desimal yang akan disimpan di sudut
 						sudut[j++]=input[i];
-					} else{
+					} else{ // jika bukan akan dianggap huruf bagian dari trigonometri seperti cos/sin/tan
 						trigono[x++]=input[i];
 					}
 					i++;
 				}
-				if(input[i]!=')'){
+				if(input[i]!=')'){ // jika tidak ada tanda kurung tutup akan kembali ke menu utama
 					*kembali='y';
 				}
-				trigono[x]='\0';
-				sudut[j]='\0';
-				angka=strtod(sudut, NULL);
-				hasil=operasi_trigono(angka,trigono,&*kembali);
-				EnqueOperand(&*listQ, hasil);	
+				trigono[x]='\0'; // dimaksud untuk mengakhiri dari string trigono dengan menambahkan karakter null ke array trigono
+				sudut[j]='\0'; // dimaksud untuk mengakhiri dari string sudut dengan menambahkan karakter null ke array sudut
+				angka=strtod(sudut, NULL); // mengubah nilai yang ada di sudut menjadi float dan ditampung di angka
+				hasil=operasi_trigono(angka,trigono,&*kembali); // memanggil modul dari header ikhsan.h untuk mengoprasikan trigonometri dan mendapatkan nilai hasilnya
+				EnqueOperand(&*listQ, hasil);	// menyimpan hasil ke queue
 			}else{
-				angka1=DequeOperand(&*listQ);
+				/*Jika karakter sebelum operator trigonometri adalah digit, berarti operator trigonometri tersebut menerima operand dari antrian operand */
+				angka1=DequeOperand(&*listQ); // mengambil operand sebelum trigono dan disimpan ke angka1
 				while(input[i]!=')'){
-					if(isdigit(input[i]) || input[i]=='.'){
+						//  loop akan membaca karakter-karakter setelah operator trigonometri hingga menemui tanda kurung tutup (')') atau akhir dari input.
+					if(isdigit(input[i]) || input[i]=='.'){ // Mengecek apakah karakter saat ini merupakan digit atau desimal yang akan disimpan di sudut
 						sudut[j++]=input[i];
-					} else{
+					} else{ // jika bukan akan dianggap huruf bagian dari trigonometri seperti cos/sin/tan
 						trigono[x++]=input[i];
 					}
 					i++;
 				}
-				trigono[x]='\0';
-				sudut[j]='\0';
-				angka=strtod(sudut, NULL);
-				hasil=operasi_trigono(angka,trigono,&*kembali);
-				hasil*=angka1;
-				EnqueOperand(&*listQ, hasil);
+				trigono[x]='\0'; // dimaksud untuk mengakhiri dari string trigono dengan menambahkan karakter null ke array trigono
+				sudut[j]='\0'; // dimaksud untuk mengakhiri dari string sudut dengan menambahkan karakter null ke array sudut
+				angka=strtod(sudut, NULL); // mengubah nilai yang ada di sudut menjadi float dan ditampung di angka
+				hasil=operasi_trigono(angka,trigono,&*kembali); // memanggil modul dari header ikhsan.h untuk mengoprasikan trigonometri dan mendapatkan nilai hasilnya
+				hasil*=angka1; // mengalikan operand sebelum logaritma dengan hasil
+				EnqueOperand(&*listQ, hasil); // menyimpan hasil ke queue
 			}
+			// Jika token merupakan operator matematika , Stack tidak kosong dan pada Head stack bukan tanda '(' dan ada bilangan sebelum operator
 		}else if(isOperator(token)&&listS.Head!=NULL&&listS.Head->oprtr!='('&&isdigit(input[i-1])){
-			oprtr=listS.Head->oprtr;
+			oprtr=listS.Head->oprtr; // Mengambil operator yang ada di bagian paling atas dari stack
 			while(derajatOperator(token)<=derajatOperator(oprtr)&&listS.Head!=NULL&&listS.Head->oprtr!='('){
+				/* Loop ini akan berjalan selama derajat operator saat ini (token) lebih kecil atau sama dengan derajat operator di puncak stack (oprtr), 
+				stack operator tidak kosong, dan operator di puncak stack bukan tanda kurung buka ('('). Catatan derajatOperator dipanggil dari Fikri.h*/
 				EnqueOperator(&*listQ,PopStack(&listS));
 			}
-			listS=PushStack(listS,token);
+			listS=PushStack(listS,token); // Melakukan push stackt atau menyimpan node baru  ke stack menggunakan modul dari Afyar.h
 		}else if(token==')'){
 			oprtr=listS.Head->oprtr;
 			while(oprtr!='('&&listS.Head->next!=NULL){
+				// Melakukan looping selama operator di puncak stack (oprtr) bukan tanda kurung buka '(' dan stack operator memiliki lebih dari satu elemen
 				EnqueOperator(&*listQ,PopStack(&listS));
 				oprtr=listS.Head->oprtr;
 			}
-			if(oprtr=='('){
-				PopStack(&listS);
+			if(oprtr=='('){ // Setelah looping mengecek apakah operator di head stack tanda '(' jika iya akan melakukan Pop stack jika bukan akan menampilkan format salah
+				PopStack(&listS); //PopStack menggunakan modul dari Afyar.h
 			}else{
 				printf("format yang dimasukkan salah\n");
 				break;
@@ -234,39 +277,43 @@ void proses_kalkulator(Queue *listQ, char* input, char* kembali){
 				EnqueOperand(&*listQ, hasil);
 			}
 
-		}else if(token=='('){
+		}else if(token=='('){ // token ialah tanda kurung buka maka dimasukkan ke stack
 			listS=PushStack(listS,token);
 		}else if(token=='!'){
-			float a,c;
+			float numFaktor,hasil;
 			char t;
 			t=token;
-			if(isdigit(input[i-1])){
-				a=DequeOperand(&*listQ);
-				c=faktorial(a);
-				EnqueOperand(&*listQ,c);
-				
+			// Mengecek Jika operand sebelumnya adalah bilangan
+			if (isdigit(input[i - 1])){ 
+				numFaktor = DequeOperand(&*listQ); // mengambil nilai dari bilangan sebelum karakter ! menggunakan modul Deque
+				hasil = faktorial(numFaktor);  // Mengambil hasil proses faktorial dari modul faktorial() dari Fikri.h
+				EnqueOperand(&*listQ, hasil);
 			}else{
 				*kembali='y';
 			}
 		}
 		else if(token=='v'&&!isdigit(input[i-1])){
+			// memeriksa apakah token saat ini adalah operator akar kuadrat, yaitu 'v'.  dan apakah karakter sebelum operator akar kuadrat bukan digit
 			i++;
 			float hasilakarkuadrat=0;
 			int plus=0;
 			char num[20];
 			float num1;
 			while(isdigit(input[i])||input[i]=='.'){
+				// Selama  karakter saat ini adalah digit atau titik desimal, loop akan berjalan untuk membaca karakter-karakter tersebut dan menyimpannya dalam array num.
 				num[plus++]=input[i];
 				i++;
 			}
-			num1=strtof(num,NULL);
-			hasilakarkuadrat=hitung_akar(num1,2);
-			EnqueOperand(&*listQ,hasilakarkuadrat);
+			num1=strtof(num,NULL); // mengubah nilai num menjadi float
+			hasilakarkuadrat=hitung_akar(num1,2); // melakukan operasi hitung akar yang diambil dari model di Afyar.h
+			EnqueOperand(&*listQ,hasilakarkuadrat); //memasukkan hasilnya kedalam queue
 		}else{
+			// Jika token bukan karakter di atas, diasumsikan sebagai operator matematika dan dimasukkan ke dalam Stack
 			listS=PushStack(listS,token);
 		}
 	}
 	while(listS.Head!=NULL){
+		// Mengosongkan Stack ke dalam Queue dengan melakukan looping sampai isi Stack kosong atau head bernilai Null
 		oprtr=PopStack(&listS);
 		EnqueOperator(&*listQ,oprtr);
 	}
